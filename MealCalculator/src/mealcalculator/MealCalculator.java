dbbeff2529a37ca36b59bf8c71571fec873838d9
Christopher Cannon <CCannon@c3-macbookpro.ncat.edu>
2017-04-26 14:16:06 -0400

    Create JavaFX app from file

--- MealCalculator/src/mealcalculator/MealCalculator.java
+++ MealCalculator/src/mealcalculator/MealCalculator.java
@@ -5,17 +5,32 @@
  */
 package mealcalculator;
 
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
 /**
  *
  * @author CCannon
  */
-public class MealCalculator {
+public class MealCalculator extends Application implements EventHandler{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void handle(Event event) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
