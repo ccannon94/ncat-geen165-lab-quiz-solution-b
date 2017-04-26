a71260f80f994eaa5caf2bf875b6fea6c6d5294f
Christopher Cannon <CCannon@c3-macbookpro.ncat.edu>
2017-04-26 14:32:06 -0400

    Create nonfunctional gui

--- MealCalculator/src/mealcalculator/MealCalculator.java
+++ MealCalculator/src/mealcalculator/MealCalculator.java
@@ -8,6 +8,13 @@ package mealcalculator;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -25,7 +32,35 @@ public class MealCalculator extends Application implements EventHandler{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BorderPane pane = new BorderPane();
+        
+        Label title = new Label("Bill Calculator");
+        pane.setTop(title);
+        
+        GridPane dataPane = new GridPane();
+        dataPane.add(new Label("Menu Price:"), 0, 0);
+        TextField priceTextField = new TextField();
+        dataPane.add(priceTextField, 1, 0);
+        dataPane.add(new Label("Discount:"), 0, 1);
+        TextField discountTextField = new TextField();
+        dataPane.add(discountTextField, 1, 1);
+        dataPane.add(new Label("Tip %:"), 0, 2);
+        TextField tipTextField = new TextField();
+        dataPane.add(tipTextField, 1, 2);
+        dataPane.add(new Label("Total Bill:"), 0, 3);
+        Label totalBillLabel = new Label("$0.00");
+        dataPane.add(totalBillLabel, 1, 3);
+        pane.setCenter(dataPane);
+        
+        HBox commandPane = new HBox();
+        Button resetButton = new Button("Reset");
+        Button submitButton = new Button("Submit");
+        commandPane.getChildren().addAll(resetButton, submitButton);
+        pane.setBottom(commandPane);
+        
+        Scene calculator = new Scene(pane, 300, 200);
+        primaryStage.setScene(calculator);
+        primaryStage.show();
     }
 
     @Override
